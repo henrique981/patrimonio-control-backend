b9931cf3bae9203c9393aa2bae2dd01581f6d53d
henrique981 <cmte.bispo981@gmail.com>
2026-05-08 16:14:01 -0300

    aumenta limite payload fotos

--- src/server.js
+++ src/server.js
@@ -15,7 +15,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Patrimônio Control API funcionando!' });
